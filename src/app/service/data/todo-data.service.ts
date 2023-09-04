bdcdbb3d44995e19154702396a0b922fd8c520f3
Preetam <nihal@Nihal.local>
2023-09-04 21:39:59 +0530

    added constant urls

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,34 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { baseUrl } from 'src/app/app.constants';
 import { Todo } from 'src/app/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoDataService {
-  baseUrl: `http://localhost:8080/users/` | undefined;
 
   constructor(private http:HttpClient) { }
 
   retrieveAllTodos(username:string){
     debugger
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`)
+    return this.http.get<Todo[]>(`${baseUrl}/users/${username}/todos`)
   }
   deleteTodo(username:any,id:any){
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`)
+    return this.http.delete(`${baseUrl}/users/${username}/todos/${id}`)
   }
   retrieveTodo(username:any,id:any){
-    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`)
+    return this.http.get<Todo>(`${baseUrl}/users/${username}/todos/${id}`)
   }
   updateTodo(username: any, id: any, todo: Todo) {
     return this.http.put(`
-    http://localhost:8080/users/${username}/todos/${id}`
+    ${baseUrl}/users/${username}/todos/${id}`
       , todo);
   }
 
   createTodo(username: any, todo: Todo) {
     return this.http.post(
-      `http://localhost:8080/users/${username}/todos`
+      `${baseUrl}/users/${username}/todos`
       ,todo);
   }
 }
--- src/app/service/service/basic-authentication.service.ts
+++ src/app/service/service/basic-authentication.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+import { baseUrl } from 'src/app/app.constants';
+
+export const TOKEN = 'token'
+export const AUTHENTICATED_USER = 'authenticaterUser'
 
 @Injectable({
   providedIn: 'root',
@@ -18,11 +22,11 @@ export class BasicAuthenticationService {
     console.log('sdsdsd' + headers);
 
     return this.http
-      .get<AuthenticationBean>(`http://localhost:8080/basicauth`,{ headers })
+      .get<AuthenticationBean>(`${baseUrl}/basicauth`,{ headers })
       .pipe(
         map((data) => {
           sessionStorage.setItem('authenticaterUser', username);
-          sessionStorage.setItem('token', basicAuthHeaderString);
+          sessionStorage.setItem(TOKEN, basicAuthHeaderString);
 
           return data;
         })
@@ -47,10 +51,10 @@ export class BasicAuthenticationService {
   getAuthenticatedToken() {
     debugger
     if (this.getAuthenticatedUser()) 
-      return sessionStorage.getItem('token');
+      return sessionStorage.getItem(TOKEN);
     
-    console.log(sessionStorage.getItem('token'))
-    return sessionStorage.getItem('token');
+    console.log(sessionStorage.getItem(TOKEN))
+    return sessionStorage.getItem(TOKEN);
   }
 
   getAuthenticatedUser() {
@@ -58,7 +62,7 @@ export class BasicAuthenticationService {
   }
   logout() {
     sessionStorage.removeItem('authenticaterUser');
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem(TOKEN);
   }
 }
 export class AuthenticationBean {
